8dbc2ecce986f90fc73a16c75be4e9cff6e35c69
Pon <steeevie22@gmail.com>
2024-08-10 19:57:54 -0700

    TruckSim: Update code to send relevant keystrokes

--- TruckSim/TruckSim_Arduino/TruckSim_Arduino.ino
+++ TruckSim/TruckSim_Arduino/TruckSim_Arduino.ino
@@ -1,54 +1,102 @@
 #include <debounce.h>
+#include <Keyboard.h>
 
-enum pin_names{
-  BUTTON_1 = 2,
-  BUTTON_2,
-  BUTTON_3,
-  BUTTON_4,
-  BUTTON_5,
-  BUTTON_6
+enum gpioAssignments{
+  GPIO_BUTTON_1 = 2,
+  GPIO_BUTTON_2,
+  GPIO_BUTTON_3,
+  GPIO_BUTTON_4,
+  GPIO_BUTTON_5,
+  GPIO_BUTTON_6
 };
 
+
+enum idAssignments{
+  ID_BUTTON_1 = 1,
+  ID_BUTTON_2,
+  ID_BUTTON_3,
+  ID_BUTTON_4,
+  ID_BUTTON_5,
+  ID_BUTTON_6
+};
+
+
+bool buttonStates[] = {false, false, false, false, false, false};
 
 static void buttonHandler(uint8_t btnId, uint8_t btnState) {
   if (btnState == BTN_PRESSED) {
     Serial.println((String)"Pushed button " + btnId);
+    buttonStates[btnId-1] = true;
+    // See https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
+    switch (btnId){
+      case ID_BUTTON_1:
+        Keyboard.press('i');
+        Keyboard.releaseAll();
+      break;
+      case ID_BUTTON_2:
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('b');
+        Keyboard.releaseAll();
+      break;
+      case ID_BUTTON_3:
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('c');
+        Keyboard.releaseAll();
+      break;
+      case ID_BUTTON_4:
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('d');
+        Keyboard.releaseAll();
+      break;
+      case ID_BUTTON_5:
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('e');
+        Keyboard.releaseAll();
+      break;
+      case ID_BUTTON_6:
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('f');
+        Keyboard.releaseAll();
+      break;
+    }
   } else {
     // btnState == BTN_OPEN.
     Serial.println((String)"Released button " + btnId);
+    buttonStates[btnId-1] = false;
   }
 }
 
 
-static Button buttonOne(1, buttonHandler);
-static Button buttonTwo(2, buttonHandler);
-static Button buttonThree(3, buttonHandler);
-static Button buttonFour(4, buttonHandler);
-static Button buttonFive(5, buttonHandler);
-static Button buttonSix(6, buttonHandler);
+static Button buttonOne(ID_BUTTON_1, buttonHandler);
+static Button buttonTwo(ID_BUTTON_2, buttonHandler);
+static Button buttonThree(ID_BUTTON_3, buttonHandler);
+static Button buttonFour(ID_BUTTON_4, buttonHandler);
+static Button buttonFive(ID_BUTTON_5, buttonHandler);
+static Button buttonSix(ID_BUTTON_6, buttonHandler);
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(BUTTON_1, INPUT_PULLUP);
-  pinMode(BUTTON_2, INPUT_PULLUP);
-  pinMode(BUTTON_3, INPUT_PULLUP);
-  pinMode(BUTTON_4, INPUT_PULLUP);
-  pinMode(BUTTON_5, INPUT_PULLUP);
-  pinMode(BUTTON_6, INPUT_PULLUP);
+  Serial.begin(9600);
+  Keyboard.begin();
+  pinMode(GPIO_BUTTON_1, INPUT_PULLUP);
+  pinMode(GPIO_BUTTON_2, INPUT_PULLUP);
+  pinMode(GPIO_BUTTON_3, INPUT_PULLUP);
+  pinMode(GPIO_BUTTON_4, INPUT_PULLUP);
+  pinMode(GPIO_BUTTON_5, INPUT_PULLUP);
+  pinMode(GPIO_BUTTON_6, INPUT_PULLUP);
 }
 
 
 static void pollButtons() {
   // update() will call buttonHandler() if PIN transitions to a new state and stays there
   // for multiple reads over 25+ ms.
-  buttonOne.update(digitalRead(BUTTON_1));
-  buttonTwo.update(digitalRead(BUTTON_2));
-  buttonThree.update(digitalRead(BUTTON_3));
-  buttonFour.update(digitalRead(BUTTON_4));
-  buttonFive.update(digitalRead(BUTTON_5));
-  buttonSix.update(digitalRead(BUTTON_6));
+  buttonOne.update(digitalRead(GPIO_BUTTON_1));
+  buttonTwo.update(digitalRead(GPIO_BUTTON_2));
+  buttonThree.update(digitalRead(GPIO_BUTTON_3));
+  buttonFour.update(digitalRead(GPIO_BUTTON_4));
+  buttonFive.update(digitalRead(GPIO_BUTTON_5));
+  buttonSix.update(digitalRead(GPIO_BUTTON_6));
 }
 
 
